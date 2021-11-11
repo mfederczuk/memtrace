1b60714a1862773e9fe32708e13afe4806b5bbdd
Michael Federczuk <federczuk.michael@protonmail.com>
2021-11-11 21:10:30 +0100

    Use correct format args for `size_t`

--- src/memtrace.c
+++ src/memtrace.c
@@ -17,7 +17,7 @@ extern "C" {
 
 void* memtrace_internal_malloc(size_t size, const char* file, int line) {
 	void* ret_ptr = malloc(size);
-	fprintf(stderr, "%s:%d: malloc(%zd): %p\n", file, line, size, ret_ptr);
+	fprintf(stderr, "%s:%d: malloc(%zu): %p\n", file, line, size, ret_ptr);
 	return ret_ptr;
 }
 
@@ -28,13 +28,13 @@ void memtrace_internal_free(void* ptr, const char* file, int line) {
 
 void* memtrace_internal_calloc(size_t nmemb, size_t size, const char* file, int line) {
 	void* ret_ptr = calloc(nmemb, size);
-	fprintf(stderr, "%s:%d: calloc(%zd, %zd): %p\n", file, line, nmemb, size, ret_ptr);
+	fprintf(stderr, "%s:%d: calloc(%zu, %zu): %p\n", file, line, nmemb, size, ret_ptr);
 	return ret_ptr;
 }
 
 void* memtrace_internal_realloc(void* ptr, size_t size, const char* file, int line) {
 	void* ret_ptr = realloc(ptr, size);
-	fprintf(stderr, "%s:%d: realloc(%p, %zd): %p\n", file, line, ptr, size, ret_ptr);
+	fprintf(stderr, "%s:%d: realloc(%p, %zu): %p\n", file, line, ptr, size, ret_ptr);
 	return ret_ptr;
 }
 
