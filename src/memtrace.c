5c099768b5d54da7f57ef1f55b70fc744637b34b
Michael Federczuk <federczuk.michael@protonmail.com>
2021-11-12 19:59:47 +0100

    Print func name and params before func call

--- src/memtrace.c
+++ src/memtrace.c
@@ -17,35 +17,41 @@ extern "C" {
 #endif
 
 void* memtrace3_internal_malloc(size_t size, const char* file, int line) {
+	fprintf(stderr, "%s:%d: malloc(%zu)", file, line, size);
+
 	void* ret_ptr = malloc(size);
 	const int local_errno = errno;
 
-	fprintf(stderr, "%s:%d: malloc(%zu): %p\n", file, line, size, ret_ptr);
+	fprintf(stderr, ": %p\n", ret_ptr);
 
 	errno = local_errno;
 	return ret_ptr;
 }
 
 void memtrace3_internal_free(void* ptr, const char* file, int line) {
-	free(ptr);
 	fprintf(stderr, "%s:%d: free(%p)\n", file, line, ptr);
+	free(ptr);
 }
 
 void* memtrace3_internal_calloc(size_t nmemb, size_t size, const char* file, int line) {
+	fprintf(stderr, "%s:%d: calloc(%zu, %zu)", file, line, nmemb, size);
+
 	void* ret_ptr = calloc(nmemb, size);
 	const int local_errno = errno;
 
-	fprintf(stderr, "%s:%d: calloc(%zu, %zu): %p\n", file, line, nmemb, size, ret_ptr);
+	fprintf(stderr, ": %p\n", ret_ptr);
 
 	errno = local_errno;
 	return ret_ptr;
 }
 
 void* memtrace3_internal_realloc(void* ptr, size_t size, const char* file, int line) {
+	fprintf(stderr, "%s:%d: realloc(%p, %zu)", file, line, ptr, size);
+
 	void* ret_ptr = realloc(ptr, size);
 	const int local_errno = errno;
 
-	fprintf(stderr, "%s:%d: realloc(%p, %zu): %p\n", file, line, ptr, size, ret_ptr);
+	fprintf(stderr, ": %p\n", ret_ptr);
 
 	errno = local_errno;
 	return ret_ptr;
